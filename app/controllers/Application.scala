6944ecb9c51939b6bde3b0098a731817dac74777
mather <mather314@gmail.com>
2012-10-23 00:17:34 +0900

    Initial commit.

---
+++ app/controllers/Application.scala
@@ -0,0 +1,20 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object Application extends Controller {
+
+  def index = Action {
+    //Ok(views.html.index("Your new application is ready."))
+    //Ok("Hello,world!")
+    Redirect(routes.Application.tasks)
+  }
+
+  def tasks = TODO
+
+  def newTask = TODO
+
+  def deleteTask(id: Long) = TODO
+
+}
